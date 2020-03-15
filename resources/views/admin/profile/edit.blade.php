29e64608673bc70b5ca4cebd8b7b956e239a64ea
kazukazufx <kazuya.tk02@gmail.com>
2020-03-15 09:47:10 +0000

    Laravel10 blade

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf=8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta nema="viewport" content="width=device-width, initial-scale=1">
+            
+            <title>Mynews</title>
+        </head>
+        <body>
+            <h1>Myニュース変更画面!!!</h1> 
+        </body>
+</html>
