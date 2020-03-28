d9379d859255467faa85870cd4ffaeb579e2e843
kazukazufx <kazuya.tk02@gmail.com>
2020-03-28 09:36:29 +0000

    kadai auth

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,13 @@
-<DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf=8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta nema="viewport" content="width=device-width, initial-scale=1">
-            
-            <title>Mynews</title>
-        </head>
-        <body>
-            <h1>Myニュース変更画面!!!</h1> 
-        </body>
-</html>
+@extends('layouts.profile')
+
+    @section('title','プロフィール編集')
+    
+        @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール編集</h2>
+                </div>
+            </div>
+        </div>
+    @endsection
