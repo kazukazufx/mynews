3e8d5ebc2affa3afc2395b8f765f09ce90e4f40c
kazukazufx <kazuya.tk02@gmail.com>
2020-04-05 09:10:22 +0000

    rireki_hozon

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -42,7 +42,7 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-md-10">
-                            <input type="hidden" name="id" value="{{ $news_form->id }}">
+                            <input type="hidden" name="news_id" value="{{ $news_form->id }}">
                             {{ csrf_field() }}
                             <input type="submit" class="btn btn-primary" value="更新">
                         </div>
